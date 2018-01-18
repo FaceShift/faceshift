46183908201ca55028b08b1659db8868d6257196
Jake Mathews <mathewsj2@wit.edu>
2018-01-18 12:28:16 -0500

    Formatting

--- main.js
+++ main.js
@@ -11,9 +11,12 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
