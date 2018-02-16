0da6aad4311dd027652a8921d76515bf288d2b87
Shirshak Sharma <shirshak.sharma@gmail.com>
2018-02-15 23:42:17 -0500

    got electron to render a React component

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from "./views/home";
+
+ReactDOM.render(<Home />, document.getElementById('root'));
---
+++ src/views/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Home extends React.Component{
+
+  render(){
+    return(
+      <div>
+        React woooo
+      </div>
+    );
+  }
+}
+
+export default Home;
