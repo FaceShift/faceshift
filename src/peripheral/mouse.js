16700319d9a89f9a9583c3bf6b1e4c2617319f9e
Shirshak Sharma <shirshak.sharma@gmail.com>
2018-02-15 23:22:38 -0500

    fixed the bug where the cursor doesn't move on corners?

--- src/peripheral/mouse.js
+++ src/peripheral/mouse.js
@@ -8,7 +8,7 @@ let mousePos = robot.getMousePos();
 
 // Move in the horizontal direction
 moveLeftRight = (pixels) => {
-  if(mousePos.x <= maxWidth && mousePos.x >= 0){
+  if(mousePos.x + pixels <= maxWidth && mousePos.x + pixels >= 0){
     mousePos = robot.getMousePos();
     robot.moveMouse(mousePos.x + pixels, mousePos.y);
   }
@@ -16,12 +16,10 @@ moveLeftRight = (pixels) => {
 
 // Move in the vertical direction
 moveUpDown = (pixels) => {
-  if(mousePos.y <= maxHeight && mousePos.y >= 0){
+  if(mousePos.y + pixels <= maxHeight && mousePos.y + pixels >= 0){
     mousePos = robot.getMousePos();
     robot.moveMouse(mousePos.x, mousePos.y + pixels);
   }
 }
-
-//setInterval(() => moveUpDown(-10), 1000);
 
 module.exports = { moveUpDown, moveLeftRight };
