37415748950f508da399dceb236b16ab2d17fea4
Jake Mathews <mathewsj2@wit.edu>
2018-02-26 09:29:54 -0500

    Changed voice api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 let webcam = require("./webcam/webcam");
+let voice = require("./voice/voice")
 let trackerSetup = require("./tracking/setup");
 
 webcam.startCamera();
 webcam.onStreamDimensionsAvailable(trackerSetup.startTracker);
+
+voice.start();
--- src/voice/voice.js
+++ src/voice/voice.js
@@ -1,64 +1,50 @@
-module.exports =  {
-  connect: function(apiKey) {
-    var request = require('request');
-    var authenticationUrl = "https://speech.googleapis.com/v1/speech:recognize?key=" + apiKey
-    request.post(authenticationUrl, function (error, response, body) {
-      console.log('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', body); // Print the HTML for the Google homepage.
-    });
+const record = require('node-record-lpcm16');
+const speech = require('@google-cloud/speech');
+
+module.exports = {
+  processResults: function (results) {
+    console.log('Transcription: ${result}\n');
   },
-
-  record: function() {
-    const record = require('node-record-lpcm16');
-
-    // Imports the Google Cloud client library
-    const speech = require('@google-cloud/speech');
-  
-    // Creates a client
-    const client = new speech.SpeechClient();
-  
-    /**
-     * TODO(developer): Uncomment the following lines before running the sample.
-     */
-    // const encoding = 'Encoding of the audio file, e.g. LINEAR16';
-    // const sampleRateHertz = 16000;
-    // const languageCode = 'BCP-47 language code, e.g. en-US';
-  
+  start: function () {
+    // The audio file's encoding, sample rate in hertz, and BCP-47 language code
     const request = {
       config: {
-        encoding: encoding,
-        sampleRateHertz: sampleRateHertz,
-        languageCode: languageCode,
+        auth: process.env.VOICE_API_KEY,
+        encoding: 'LINEAR16',
+        sampleRateHertz: 16000,
+        languageCode: 'en-US',
       },
       interimResults: false, // If you want interim results, set this to true
     };
-  
+
+    // Creates a client
+    const client = new speech.SpeechClient();
+
     // Create a recognize stream
     const recognizeStream = client
       .streamingRecognize(request)
       .on('error', console.error)
-      .on('data', data =>
-        process.stdout.write(
-          data.results[0] && data.results[0].alternatives[0] ?
-          `Transcription: ${data.results[0].alternatives[0].transcript}\n` :
-          `\n\nReached transcription time limit, press Ctrl+C\n`
-        )
-      );
-  
+      .on('data', data => {
+        const hasResults = data.results[0] && data.results[0].alternatives[0];
+        if (hasResults) {
+          const result = data.results[0].alternatives[0];
+          processResults(result);
+        } else {
+          console.log('\nTime limit reached\n');
+        }
+      });
+
     // Start recording and send the microphone input to the Speech API
-    record
-      .start({
-        sampleRateHertz: sampleRateHertz,
+    record.start({
+        sampleRateHertz: request.config.sampleRateHertz,
         threshold: 0,
-        // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
         verbose: false,
         recordProgram: 'rec', // Try also "arecord" or "sox"
         silence: '10.0',
       })
       .on('error', console.error)
       .pipe(recognizeStream);
-  
+
     console.log('Listening, press Ctrl+C to stop.');
   }
 }
