671e4eceff23f5900c508e86a7bccf7e1153656a
Jake Mathews <mathewsj2@wit.edu>
2018-03-21 12:29:57 -0400

    Fixed encoding issue

--- src/voice/voice2.js
+++ src/voice/voice2.js
@@ -56,6 +56,7 @@ function trainVoiceModel(waveFilePaths) {
   var url = 'https://snowboy.kitt.ai/api/v1/train/'
   var options = {
     method: 'post',
+    encoding: null,
     body: postData,
     json: true,
     url: url
@@ -68,7 +69,7 @@ function trainVoiceModel(waveFilePaths) {
       var modelLocation = "resources/voice-models/voice-model.pmdl"
       console.log("Model created")
       console.log("Writing voice model to " + modelLocation);
-      fs.writeFile(modelLocation, body, 'binary', function (err) {
+      fs.writeFile(modelLocation, body, 'latin1', function (err) {
         if (err) {
           console.log(err)
         } else {
